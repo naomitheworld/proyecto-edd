8840b5ce160848f9f9787c14a7c7a5033bd1366b
re_gina <garcianaomi.989@gmail.com>
2022-12-07 20:27:45 -0700

    sex

--- app/src/main/java/com/example/proyecto_edd/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/proyecto_edd/AdminSQLiteOpenHelper.java
@@ -16,7 +16,7 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table usuarios(usuario text primary key, contraseña text)");
         db.execSQL("create table apps(nombre text primary key)");
-        db.execSQL("create table conductores(id int primary key, nombre text, app text references apps(nombre))");
+        db.execSQL("create table conductores(id int primary key, nombre text, sexo text, app text references apps(nombre))");
         db.execSQL("create table review(id int primary key, app text references apps(nombre), conductor text references conductores(nombre), " +
                 "reseña text)");
     }
