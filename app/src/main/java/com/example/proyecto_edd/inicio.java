8d8318bba081ddc6a8e0ae5bdd1d4a607a8769d1
re_gina <garcianaomi.989@gmail.com>
2022-12-07 20:05:37 -0700

    activity y medio codigo

--- app/src/main/java/com/example/proyecto_edd/inicio.java
+++ app/src/main/java/com/example/proyecto_edd/inicio.java
@@ -2,13 +2,44 @@ package com.example.proyecto_edd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class inicio extends AppCompatActivity {
-
+private Spinner sp_apps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "administracion", null, 1);
+        SQLiteDatabase db = admin.getReadableDatabase();
+        Cursor c = db.rawQuery("select nombre from apps", null);
+        startManagingCursor(c);
+        sp_apps = findViewById(R.id.sp_apps);
+        String[] apps = new String[]{"nombre"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,apps);
+        sp_apps.setAdapter(adapter);
+
+        initialize();
     }
+
+    private void initialize(){
+        sp_apps.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View arg1, int arg2, long arg3) {
+                String seleccion = sp_apps.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
 }
